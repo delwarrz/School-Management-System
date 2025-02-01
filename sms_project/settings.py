e8cf5d81c57d30c9a58d3902238533cc9478d811
Delwar Hossain <delwar.ewu@gmail.com>
2025-02-01 19:06:01 +0600

    renaming main project name

--- sms_project/settings.py
+++ sms_project/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'sms.urls'
+ROOT_URLCONF = 'sms_project.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'sms.wsgi.application'
+WSGI_APPLICATION = 'sms_project.wsgi.application'
 
 
 # Database
