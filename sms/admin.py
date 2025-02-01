d454e9365432d96f2bf74ebf686f06119410a315
Delwar Hossain <delwar.ewu@gmail.com>
2025-02-02 02:52:09 +0600

    Updating columns for sms app in django admin panel

--- sms/admin.py
+++ sms/admin.py
@@ -2,6 +2,36 @@ from django.contrib import admin
 from .models import Student, Course, Enrollment
 
 # Register your models here.
-admin.site.register(Course)
-admin.site.register(Student)
-admin.site.register(Enrollment)
+
+# Showing Course CRUD in Admin Panel
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'created_at')
+
+admin.site.register(Course, CourseAdmin)
+
+
+# Showing Student CRUD in Admin Panel
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone')
+
+admin.site.register(Student, StudentAdmin)
+
+# Showing Enrollment CRUD in Admin Panel
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_student_info', 'get_course_info', 'created_at')
+
+
+    def get_student_info(self, obj):
+        return "Name: " + obj.student.name + ", Email: " + obj.student.email + ", Phone: " + obj.student.phone
+    
+    get_student_info.short_description = 'Student Info'
+
+
+    def get_course_info(self, obj):
+        return "Name: " + obj.course.name + ", Code: " + obj.course.code
+    
+    get_course_info.short_description = 'Course Info'
+
+
+
+admin.site.register(Enrollment, EnrollmentAdmin)
--- sms/apps.py
+++ sms/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SmsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sms'
+    verbose_name = 'School Management System'
